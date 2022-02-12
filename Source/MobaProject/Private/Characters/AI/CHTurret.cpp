3aac5dc5d1c87d773f9f42d296d92ecfd8b17882
Tom <tompenn18@gmail.com>
2022-02-12 18:24:29 +0000

    Test AI Blueprints & Tasks

--- Source/MobaProject/Private/Characters/AI/CHTurret.cpp
+++ Source/MobaProject/Private/Characters/AI/CHTurret.cpp
@@ -3,3 +3,161 @@
 
 #include "Characters/AI/CHTurret.h"
 
+#include "Components/CharacterNamePlate.h"
+#include "Net/UnrealNetwork.h"
+#include "Widgets/Components/WNamePlate.h"
+DEFINE_LOG_CATEGORY(LogCHTurret);
+
+// Sets default values
+ACHTurret::ACHTurret()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	AbilitySystemComponent = CreateDefaultSubobject<UCHAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	TowerMesh = CreateDefaultSubobject<UStaticMeshComponent>("TowerMesh");
+	SetRootComponent(TowerMesh);
+
+	NamePlateComponent = CreateDefaultSubobject<UCharacterNamePlate>("NamePlateComponent");
+	const FAttachmentTransformRules AttachmentRules(EAttachmentRule::KeepRelative, false);
+	NamePlateComponent->AttachToComponent(GetRootComponent(), AttachmentRules);
+
+	Attributes = CreateDefaultSubobject<UCHAttributeSet>("Attributes");
+}
+
+void ACHTurret::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACHTurret, Team);
+}
+
+FName ACHTurret::GetEntityName()
+{
+	return FName("Turret");
+}
+
+int32 ACHTurret::GetCharacterLevel() const
+{
+	return 1;
+}
+
+void ACHTurret::InitializeAttributes()
+{
+	if (!HasAuthority()) return;
+
+	if (!AbilitySystemComponent)
+	{
+		UE_LOG(LogCHTurret, Error, TEXT("AbilitySystemComponent not valid"));
+		return;
+	}
+	if (!DefaultAttributeEffect)
+	{
+		UE_LOG(LogCHTurret, Error, TEXT("DefaultAttributeEffect not valid"));
+		return;
+	}
+
+	const FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	const FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(
+		DefaultAttributeEffect, GetCharacterLevel(), ContextHandle);
+
+	if (SpecHandle.IsValid())
+	{
+		AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	}
+}
+
+void ACHTurret::InitializeOwningActor()
+{
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
+
+void ACHTurret::InitializeAbilityBinds()
+{
+}
+
+void ACHTurret::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	if (AbilitySystemComponent)
+	{
+		InitializeOwningActor();
+		InitializeAttributes();
+	}
+}
+
+void ACHTurret::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	if (AbilitySystemComponent && InputComponent)
+	{
+		InitializeOwningActor();
+		InitializeAbilityBinds();
+	}
+}
+
+void ACHTurret::HandleDamage(float Damage, const FHitResult HitInfo, const FGameplayTagContainer& DamageTags,
+                             ACHBase* InstigatorCharacter, AActor* DamageCauser)
+{
+	BP_OnDamaged(Damage, HitInfo, DamageTags, InstigatorCharacter, DamageCauser);
+}
+
+void ACHTurret::HandleHealthChanged(float DeltaValue, const FGameplayTagContainer& EventTags)
+{
+	if (!HasAuthority()) return;
+	BP_OnHealthChanged(DeltaValue, EventTags);
+	if (Attributes->GetHealth() == 0.f)
+	{
+		Destroy();
+	}
+}
+
+void ACHTurret::OnRep_Attribute(const FGameplayAttribute& Attribute, const FGameplayAttributeData& OldValue,
+                                const FGameplayAttributeData& NewValue)
+{
+	UWNamePlate* NamePlate = NamePlateComponent->GetNamePlateWidget();
+	if (!NamePlate) { return; }
+
+	if (Attribute == Attributes->GetMaxHealthAttribute() || Attribute == Attributes->GetHealthAttribute())
+	{
+		NamePlate->SetHealthPercentage(Attributes->GetHealthPercent());
+	}
+	else if (Attribute == Attributes->GetManaAttribute() || Attribute == Attributes->GetMaxManaAttribute())
+	{
+		NamePlate->SetManaPercentage(Attributes->GetManaPercent());
+	}
+}
+
+UAbilitySystemComponent* ACHTurret::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ACHTurret::SetupNamePlateWidget()
+{
+	if (NamePlateComponent)
+	{
+		UWNamePlate* NamePlate = NamePlateComponent->GetNamePlateWidget();
+		if (NamePlate)
+		{
+			NamePlate->HideLevel();
+			NamePlate->HideMana();
+			NamePlate->SetHealthPercentage(Attributes->GetHealthPercent());
+			NamePlate->SetName(GetEntityName());
+		}
+	}
+}
+
+// Called when the game starts or when spawned
+void ACHTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	SetupNamePlateWidget();
+}
+
+// Called to bind functionality to input
+void ACHTurret::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
--- Source/MobaProject/Public/Characters/AI/CHTurret.h
+++ Source/MobaProject/Public/Characters/AI/CHTurret.h
@@ -3,15 +3,124 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CHNonPlayable.h"
+#include "AbilitySystemInterface.h"
+#include "Characters/CHAbilitySystemComponent.h"
+#include "Characters/CHAttributeSet.h"
+#include "GameFramework/Pawn.h"
+#include "MobaProject/MobaProject.h"
 #include "CHTurret.generated.h"
+DECLARE_LOG_CATEGORY_EXTERN(LogCHTurret, Log, All);
 
-/**
- * 
- */
-UCLASS(Blueprintable)
-class MOBAPROJECT_API ACHTurret : public ACHNonPlayable
+class UCharacterNamePlate;
+
+UCLASS()
+class MOBAPROJECT_API ACHTurret : public APawn, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
-	
+public:
+	ACHTurret();
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE ETeam GetTeam() const { return Team; }
+
+	/**
+	* Team this unit belongs too
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated)
+	ETeam Team;
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	////////// Ability System //////////
+
+	/**
+	 * AbilitySystemComponent for this Character
+	 */
+	UPROPERTY
+	(VisibleAnywhere, BlueprintReadOnly, Category="Ability", meta=(AllowPrivateAccess=true))
+	UCHAbilitySystemComponent* AbilitySystemComponent;
+
+	/**
+	 * The Attribute set for this Character
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Ability", meta=(AllowPrivateAccess=true))
+	UCHAttributeSet* Attributes;
+
+	/**
+	 * This Characters default attributes for when its spawned or levels up
+	 */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category="Attribute")
+	TSubclassOf<UGameplayEffect> DefaultAttributeEffect;
+
+	/**
+	 * Name plate component to show character health / mana level and entity name
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UCharacterNamePlate* NamePlateComponent;
+
+	/**
+	 * Used to set the NamePlateComponent Name. Ex PlayerName, or for Ai 'Minion' or 'Turret'
+	 */
+	virtual FName GetEntityName();
+
+	/**
+	 * Get this Characters level to know what attribute values to spawn it with
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	virtual int32 GetCharacterLevel() const;
+
+private:
+	/**
+	 * Initialize this characters attributes and owner
+	 */
+	virtual void InitializeAttributes();
+	virtual void InitializeOwningActor();
+	virtual void InitializeAbilityBinds();
+public:
+	/**
+	 * This only happens on server 
+	 */
+	virtual void PossessedBy(AController* NewController) override;
+
+	/**
+	 * This happens client side
+	 */
+	virtual void OnRep_PlayerState() override;
+
+	////////// Server only  //////////
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_OnDamaged(float Damage, const FHitResult HitInfo, const FGameplayTagContainer& DamageTags,
+	                  ACHBase* InstigatorCharacter, AActor* DamageCauser);
+	UFUNCTION(BlueprintImplementableEvent)
+	void BP_OnHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+	virtual void HandleDamage(float Damage, const FHitResult HitInfo, const FGameplayTagContainer& DamageTags,
+	                          ACHBase* InstigatorCharacter, AActor* DamageCauser);
+	virtual void HandleHealthChanged(float DeltaValue, const struct FGameplayTagContainer& EventTags);
+	////////// Server only //////////
+
+	/**
+	* Client only Attribute On_Rep
+	*/
+	virtual void OnRep_Attribute(const FGameplayAttribute& Attribute, const FGameplayAttributeData& OldValue,
+	                             const FGameplayAttributeData& NewValue);
+
+	friend UCHAttributeSet;
+
+	FORCEINLINE UCHAttributeSet* GetAttributeSet() const { return Attributes; }
+	FORCEINLINE UCharacterNamePlate* GetNamePlateComponent() const { return NamePlateComponent; }
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+protected:
+	virtual void SetupNamePlateWidget();
+
+	////////// Ability System //////////
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Turret")
+	UStaticMeshComponent* TowerMesh;
 };
